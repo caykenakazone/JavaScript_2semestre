a62ccad26e208998ad6d99e82c647d6f7ab6b52f
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-11-10 23:03:45 -0300

    d

--- Lista12/scripts/scripts.js
+++ Lista12/scripts/scripts.js
@@ -33,9 +33,42 @@ function validar3(){
 //----------------------------------------------------//
 function validar4(){
     var valor4 = document.getElementById("v4").value;
-    var p4 = /^(M|m)(T|t)-\d{2}.\d{3}-(I|i)(F|f)(T|t)(M|m)}$/;
+    var p4 = /(M|m)(T|t)-\d{2}\.\d{3}-(I|i)(F|f)(T|t)(M|m)}$/;
     
     if(p4.test(valor4)){
+        alert("Ok")
+    }else{
+        alert("Invalido")
+    }
+}
+//----------------------------------------------------//
+function validar5(){
+    var valor5 = document.getElementById("v1").value;
+    var p5 =/^MT-\d{2}.\d{3}-(I|i)(F|f)(T|t)(M|m)}$/;
+    
+    if(p5.test(valor5)){
+        alert("Ok")
+    }else{
+        alert("Invalido")
+    }
+}
+//----------------------------------------------------//
+function validar1(){
+    var valor1 = document.getElementById("v1").value;
+    var p1 = /\d{2}\/\d{2}\/\d{2,4}$/;
+    
+    if(p1.test(valor1)){
+        alert("Ok")
+    }else{
+        alert("Invalido")
+    }
+}
+//----------------------------------------------------//
+function validar1(){
+    var valor1 = document.getElementById("v1").value;
+    var p1 = /\d{2}\/\d{2}\/\d{2,4}$/;
+    
+    if(p1.test(valor1)){
         alert("Ok")
     }else{
         alert("Invalido")
