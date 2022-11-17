b901af394412b50aa799f388df94b38883a64099
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-11-17 13:38:40 -0300

    commit app pronta

---
+++ Lista13/js/login.js
@@ -0,0 +1,35 @@
+function validarLogin() {
+    user = document.getElementById("user").value;
+    senha = document.getElementById("senha").value;
+
+    if(!user && !senha){
+        alert("Usuário e Senha em branco, favor preencher");
+        document.getElementById("user").focus();
+    }else if(!user){
+        alert("Usuário em Branco, favor preencher")
+    }else if(!senha){
+        alert("Senha em Branco, favor preencher")
+        document.getElementById("senha").focus();
+    }else{loginProcess(user,senha);}
+}
+function loginProcess(user,senha){
+    file = "json/users.json";
+
+    fetch(file)
+        .then(response => response.json())
+        .then(content => checkUserLogin(content,user,senha))
+        .catch(err => alert("Problemas na leitura do JSON!"));
+
+    
+}
+function checkUserLogin(content,user,senha){
+    var achouUser = false;
+    for(var i=0;i<content.usuarios.length;i++){
+        if((content.usuarios[i].user == user )&&(content.usuarios[i].senha == senha)){
+            achouUser = true;
+            break;
+        }
+    }
+    if(achouUser){alert("Usuário existente");}
+    else{alert("Usuário inexistente");}
+}
